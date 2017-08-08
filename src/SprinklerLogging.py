0da64baa66f74df57b66912ee53c04bba592d9dc
rhino2rhonda <rhino2rhonda@gmail.com>
2017-08-09 00:47:11 +0530

    Relative path converted to absolute path

--- src/SprinklerLogging.py
+++ src/SprinklerLogging.py
@@ -10,6 +10,13 @@ def create_log_dir():
         os.mkdir(log_dir)
 
 
+# Converts relative paths to absolute paths
+def handle_relative_paths(log_dict):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    file_handler = log_dict['handlers']['file_handler']
+    file_handler['filename'] = os.path.join(dir_path, file_handler['filename'])
+
+
 # Configures application wide logging from JSON file
 def configure_logging():
     create_log_dir()
@@ -17,6 +24,7 @@ def configure_logging():
     log_config_file = os.path.join(dir_path, 'logging.json')
     with open(log_config_file, 'r') as log_file:
         log_dict = json.load(log_file)
+        handle_relative_paths(log_dict)
         logging.config.dictConfig(log_dict)
         logger = logging.getLogger(__name__)
         logger.info("Logging has been configured")
